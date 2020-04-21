dca87c0cd37fd55a8c2ece3f3ba67bd93f1810b3
Jordan Negreiros <jordannegreirossantos@gmail.com>
2020-04-21 20:10:08 -0300

    Mapeando a classe de entidade e adicionando o Lombok

--- src/main/java/br/com/jordan/curso/domain/Usuario.java
+++ src/main/java/br/com/jordan/curso/domain/Usuario.java
@@ -1,101 +1,44 @@
 package br.com.jordan.curso.domain;
 
+import lombok.*;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Usuario {
+@Entity
+@Table(name = "usuarios")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
+    @Column(nullable = false, length = 50)
     @NotBlank
     @Size(min = 3, max = 50)
     private String nome;
 
+    @Column(nullable = false, length = 50)
     @NotBlank
     @Size(min = 3, max = 50, message = "Campo requerido entre {min} e {max} caracteres.")
     private String sobrenome;
 
+    @Column(name = "tipo_sexo", nullable = false)
+    @Enumerated(EnumType.STRING)
     private TipoSexo sexo;
 
+    @Column(name = "data_nascimento", nullable = false)
     @NotNull(message = "O campo 'data de nascimento' é requerido.")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate DtNascimento;
 
-    public Usuario() {
-    }
-
-    public Usuario(Long id, String nome, String sobrenome) {
-        this.id = id;
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-    }
-
-    public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento) {
-        this.id = id;
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        DtNascimento = dtNascimento;
-    }
-
-    public Usuario(Long id, String nome, String sobrenome, LocalDate dtNascimento, TipoSexo sexo) {
-        this.id = id;
-        this.nome = nome;
-        this.sobrenome = sobrenome;
-        this.sexo = sexo;
-        DtNascimento = dtNascimento;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public TipoSexo getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(TipoSexo sexo) {
-        this.sexo = sexo;
-    }
-
-    public LocalDate getDtNascimento() {
-        return DtNascimento;
-    }
-
-    public void setDtNascimento(LocalDate dtNascimento) {
-        DtNascimento = dtNascimento;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Usuario{");
-        sb.append("id=").append(id);
-        sb.append(", nome='").append(nome).append('\'');
-        sb.append(", sobrenome='").append(sobrenome).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
